15e392af19fc57cce37db3d1da156a1c028de212
Jack Westbrook <jack.westbrook@gmail.com>
2023-05-24 11:22:09 +0200

    chore: delete unused index.js file

--- llm-frontend-libs/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/index.production.js');
-} else {
-  module.exports = require('./dist/index.development.js');
-}
