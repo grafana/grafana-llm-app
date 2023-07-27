531b1fb22492f098cd9dc9fc0f039b5500230140
Yasir Ekinci <yas.ekinci@grafana.com>
2023-07-27 18:40:25 +0300

    resources.go: handle ping and echo

--- pkg/plugin/resources.go
+++ pkg/plugin/resources.go
@@ -1,6 +1,7 @@
 package plugin
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
@@ -9,6 +10,41 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/resource/httpadapter"
 )
+
+// /api/plugins/app-with-backend/resources/ping
+
+// handlePing is an example HTTP GET resource that returns a {"message": "ok"} JSON response.
+func (a *App) handlePing(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	if _, err := w.Write([]byte(`{"message": "pong"}`)); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.DefaultLogger.Info("ping received")
+	w.WriteHeader(http.StatusOK)
+}
+
+// handleEcho is an example HTTP POST resource that accepts a JSON with a "message" key and
+// returns to the client whatever it is sent.
+func (a *App) handleEcho(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	var body struct {
+		Message string `json:"message"`
+	}
+	if err := json.NewDecoder(req.Body).Decode(&body); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(body); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
 
 func newOpenAIProxy() http.Handler {
 	return &httputil.ReverseProxy{
@@ -27,5 +63,7 @@ func newOpenAIProxy() http.Handler {
 
 // registerRoutes takes a *http.ServeMux and registers some HTTP handlers.
 func (a *App) registerRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("/ping", a.handlePing)
+	mux.HandleFunc("/echo", a.handleEcho)
 	mux.Handle("/openai/", newOpenAIProxy())
 }
