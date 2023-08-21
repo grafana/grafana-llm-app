bac6d6f32dd196564fbc249580ffe1cc8765b658
Aaron Sanders <aaron.sanders@grafana.com>
2023-08-21 11:59:23 -0500

    Adds indicators for started and finished requests Request is pending if started and !finished

--- llmexamples-app/src/pages/ExamplePage.tsx
+++ llmexamples-app/src/pages/ExamplePage.tsx
@@ -4,6 +4,7 @@ import { useAsync } from 'react-use';
 import { llms } from '@grafana/experimental';
 import { PluginPage } from '@grafana/runtime';
 import { Button, Input, Spinner } from '@grafana/ui';
+import { finalize } from 'rxjs';
 
 export function ExamplePage() {
   // The current input value.
@@ -12,6 +13,9 @@ export function ExamplePage() {
   const [message, setMessage] = React.useState('');
   // The latest reply from the LLM.
   const [reply, setReply] = useState('');
+
+  const [started, setStarted] = useState(false);
+  const [finished, setFinished] = useState(false)
 
   const { loading, error, value } = useAsync(async () => {
     // Check if the LLM plugin is enabled and configured.
@@ -23,6 +27,9 @@ export function ExamplePage() {
     if (message === '') {
       return { enabled };
     }
+
+    setStarted(true);
+    setFinished(false);
     // Stream the completions. Each element is the next stream chunk.
     const stream = llms.openai.streamChatCompletions({
       model: 'gpt-3.5-turbo',
@@ -33,7 +40,11 @@ export function ExamplePage() {
     }).pipe(
       // Accumulate the stream content into a stream of strings, where each
       // element contains the accumulated message so far.
-      llms.openai.accumulateContent()
+      llms.openai.accumulateContent(),
+      finalize(() => {
+        setStarted(false);
+        setFinished(true);
+      })
     );
     // Subscribe to the stream and update the state for each returned value.
     return {
@@ -60,6 +71,8 @@ export function ExamplePage() {
           <Button type="submit" onClick={() => setMessage(input)}>Submit</Button>
           <br />
           <div>{loading ? <Spinner /> : reply}</div>
+          <div>{started ? "Response is started" : "Response is not started"}</div>
+          <div>{finished ? "Response is finished" : "Response is not finished"}</div>
         </>
       ) : (
         <div>LLM plugin not enabled.</div>
