03b8257866c03dc19ebbd9cc79967b22cec0e52b
Aaron Sanders <aaron.sanders@grafana.com>
2023-10-30 14:20:04 -0500

    Extend base plugin webpack config New webpack config ignores nested projects

---
+++ webpack.config.ts
@@ -0,0 +1,20 @@
+// webpack.config.ts
+import type { Configuration } from 'webpack';
+import { merge } from 'webpack-merge';
+import grafanaConfig from './.config/webpack/webpack.config';
+
+const config = async (env): Promise<Configuration> => {
+  const baseConfig = await grafanaConfig(env);
+
+  return merge(baseConfig, {
+    // Add custom config here...
+    output: {
+      asyncChunks: true,
+    },
+    module: {
+        noParse: /(node_modules|llm-frontend-libs|llmexamples-app)/
+    }
+  });
+};
+
+export default config;
