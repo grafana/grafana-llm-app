36d74edb7ffe92c62e5345ca06032c388297d3f2
Aaron Sanders <aaron.sanders@grafana.com>
2023-12-11 16:30:25 -0600

    Switch to yarn buid

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AppRootProps } from '@grafana/data';
 import { Models } from '../../pages';
 
 export function App(props: AppRootProps) {
   return (
-    <Switch>
-      {/* Default page */}
-      <Route component={Models} />
-    </Switch>
+    <BrowserRouter>
+      <Routes>
+        {/* Default page */}
+        <Route Component={Models} />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/components/AppConfig/DevSandbox.tsx
+++ src/components/AppConfig/DevSandbox.tsx
@@ -1,14 +1,122 @@
 import React, { useState } from "react";
-import { Button, FieldSet, Modal } from "@grafana/ui";
+import { Button, FieldSet, Input, Modal, Spinner } from "@grafana/ui";
+import { useAsync } from "react-use";
+import { finalize } from "rxjs";
+import { openai } from "@grafana/llm";
+
+const BasicChatTest = () => {
+  // The current input value.
+  const [input, setInput] = useState('');
+  // The final message to send to the LLM, updated when the button is clicked.
+  const [message, setMessage] = useState('');
+  // The latest reply from the LLM.
+  const [reply, setReply] = useState('');
+
+  const [useStream, setUseStream] = useState(false);
+
+  const [started, setStarted] = useState(false);
+  const [finished, setFinished] = useState(true);
+
+  const { loading, error, value } = useAsync(async () => {
+    // Check if the LLM plugin is enabled and configured.
+    // If not, we won't be able to make requests, so return early.
+    const openAIHealthDetails = await openai.enabled();
+    const enabled = openAIHealthDetails;
+    if (!enabled) {
+      return { enabled };
+    }
+    if (message === '') {
+      return { enabled };
+    }
+
+    setStarted(true);
+    setFinished(false);
+    if (!useStream) {
+      // Make a single request to the LLM.
+      const response = await openai.chatCompletions({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          { role: 'system', content: 'You are a cynical assistant.' },
+          { role: 'user', content: message },
+        ],
+      });
+      setReply(response.choices[0].message.content);
+      setStarted(false);
+      setFinished(true);
+      return { enabled, response };
+    } else {
+      // Stream the completions. Each element is the next stream chunk.
+      const stream = openai.streamChatCompletions({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          { role: 'system', content: 'You are a cynical assistant.' },
+          { role: 'user', content: message },
+        ],
+      }).pipe(
+        // Accumulate the stream content into a stream of strings, where each
+        // element contains the accumulated message so far.
+        openai.accumulateContent(),
+        // The stream is just a regular Observable, so we can use standard rxjs
+        // functionality to update state, e.g. recording when the stream
+        // has completed.
+        // The operator decision tree on the rxjs website is a useful resource:
+        // https://rxjs.dev/operator-decision-tree.
+        finalize(() => {
+          setStarted(false);
+          setFinished(true);
+        })
+      );
+      // Subscribe to the stream and update the state for each returned value.
+      return {
+        enabled,
+        stream: stream.subscribe(setReply),
+      };
+    }
+  }, [message]);
+
+  if (error) {
+    // TODO: handle errors.
+    return <div>error</div>;
+  }
+
+  return (
+    <div>
+      {value?.enabled ? (
+        <>
+          <Input
+            value={input}
+            onChange={(e) => setInput(e.currentTarget.value)}
+            placeholder="Enter a message"
+          />
+          <br />
+          <Button type="submit" onClick={() => {setMessage(input); setUseStream(true);}}>Submit Stream</Button>
+          <Button type="submit" onClick={() => {setMessage(input); setUseStream(false);}}>Submit Request</Button>
+          <br />
+          <div>{loading ? <Spinner /> : reply}</div>
+          <div>{started ? "Response is started" : "Response is not started"}</div>
+          <div>{finished ? "Response is finished" : "Response is not finished"}</div>
+        </>
+      ) : (
+        <div>LLM plugin not enabled.</div>
+      )}
+    </div>
+  );
+};
+
 
 export const DevSandbox = () => {
     const [modalIsOpen, setModalIsOpen] = useState(false);
+    const closeModal = () => {
+      setModalIsOpen(false);
+    }
+    
 
     return (
         <FieldSet label="Development Sandbox">
             <Button onClick={() => setModalIsOpen(true)}>Open development sandbox</Button>
-                <Modal title="Development Sandbox" isOpen={modalIsOpen}>
-                    <Button variant="primary" onClick={() => setModalIsOpen(false)}>
+                <Modal title="Development Sandbox" isOpen={modalIsOpen} onDismiss={closeModal}>
+                  <BasicChatTest />
+                    <Button variant="primary" onClick={closeModal}>
                     Close
                     </Button>
                 </Modal>
