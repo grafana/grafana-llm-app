4781233d852d45e73465d604a50af4b52cef57c1
Rob Whelan <rob.whelan@grafana.com>
2024-02-17 10:51:11 +0100

    new terms text from Geoff

--- src/components/AppConfig/LLMConfig.tsx
+++ src/components/AppConfig/LLMConfig.tsx
@@ -101,15 +101,24 @@ export function LLMConfig({
             {llmOption === 'grafana-provided' && (
               <>
                 <div className={s.openaiTermsBox}>
+                  <p>To enable OpenAI via Grafana Labs, please note that some data from your Grafana instance will be sent to OpenAI when you use the LLM-based features. Grafana Labs imposes usage limits for this service.</p>
+                  <p>Additionally, the following terms (&quot;AI Terms&quot;) are hereby added to and become part of your licensing agreement with Grafana Labs (the &quot;Agreement&quot;) as additional terms. Capitalized terms not defined in these AI Terms have the meanings given in the Agreement. These terms apply to your specific use of the OpenAI via Grafana Labs feature(s), and are separate, necessary terms regarding your use of this feature and therefore are not &apos;click-wrap&apos;, &apos;shrink-wrap&apos;, different or additional terms, or the like, to the extent your licensing agreement with Grafana Labs purports to supersede any such terms.</p>
                   <ul>
-                    <li>Grafana uses OpenAI’s API platform to provide LLM functionality.</li>
-                    <li>
-                      OpenAI does not train models on inputs or outputs of their API platform. OpenAI does retain data
-                      for a short time to provide the services and monitor for abuse. All data is encrypted in transit
-                      and at rest.
+                    <li>Grafana Labs uses OpenAI&apos;s API platform to provide the LLM features.</li>
+                    <li>OpenAI does not train aggregated models on inputs or outputs of the API platform as used in connection with Grafana Labs Product(s).</li>
+                    <li>OpenAI does retain data for a short time in order to provide the services and monitor for abuse. All data sent to OpenAI is encrypted in transit and at rest.</li>
+                    <li>All features utilizing OpenAI are clearly marked in the Grafana Labs Product(s), and each feature sends minimal data to OpenAI&mdash;and only at the request of a user (for example, when a user clicks the button to request an Incident auto-summary).</li>
+                    <li>Grafana Labs will add new features regularly that utilize features connecting to OpenAI&apos;s APIs, which may include, but are not limited to:
+                      <ul>
+                        <li>Explaining Flamegraphs & offer suggestions to fix issues</li>
+                        <li>Incident auto-summary</li>
+                        <li>Suggesting names & descriptions for panels & dashboards, and summarize differences when saving changes</li>
+                        <li>Explaining error log lines in Sift</li>
+                        <li>Generating KQL queries in the Azure Data Explorer plugin</li>
+                      </ul>
                     </li>
                     <li>
-                      Visit the OpenAI trust portal for more details:{' '}
+                      Visit the OpenAI trust portal for more detail about OpenAI:{' '}
                       <Button
                         size="sm"
                         variant="secondary"
@@ -119,19 +128,17 @@ export function LLMConfig({
                       </Button>
                     </li>
                     <li>
-                      AI features are clearly marked in Grafana, and each feature sends minimal data to OpenAI, and only
-                      on user request (for example, when someone clicks the button to request an Incident auto-summary).
+                      If you enable this feature, OpenAI will be a subprocessor of Grafana Labs for the purpose of any data processing agreement you may have in place with Grafana Labs.
                     </li>
                     <li>
-                      By enabling this integration, I accept that Grafana shares limited data to the OpenAI API as
-                      needed to provide LLM-powered features.
+                      Disclaimer. Outputs are generated through machine learning processes and are not tested, verified, endorsed or guaranteed to be accurate, complete or current by Grafana Labs. You should independently review and verify all outputs as to appropriateness for any or all of your use cases or applications. The warranties, disclaimers, and limitations of liability in the Agreement apply to the AI Features.
                     </li>
                   </ul>
                 </div>
                 <Checkbox
                   value={optIn}
                   onClick={optInChange}
-                  label="I accept limited data sharing with OpenAI as described above"
+                  label="I accept the AI Terms above"
                 />
               </>
             )}
