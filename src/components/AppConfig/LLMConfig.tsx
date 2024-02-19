299baa5d69495c46896080558e9e78b3c8afd3d0
Gerry Boland <gerboland@gmail.com>
2024-02-19 14:59:30 +0000

    Update licence text & terminology

--- src/components/AppConfig/LLMConfig.tsx
+++ src/components/AppConfig/LLMConfig.tsx
@@ -89,66 +89,99 @@ export function LLMConfig({
 
   return (
     <FieldSet label="OpenAI Settings" className={s.sidePadding}>
+      {' '}
       {allowGrafanaManagedLLM && (
-        <Card
-          isSelected={llmOption === 'grafana-provided'}
-          onClick={selectGrafanaManaged}
-          className={s.cardWithoutBottomMargin}
-        >
-          <Card.Heading>Use OpenAI provided by Grafana</Card.Heading>
-          <Card.Description>
-            <p>Enable LLM features in Grafana by using a connection to OpenAI that is provided by Grafana</p>
-            {llmOption === 'grafana-provided' && (
-              <>
-                <div className={s.openaiTermsBox}>
-                  <p>To enable OpenAI via Grafana Labs, please note that some data from your Grafana instance will be sent to OpenAI when you use the LLM-based features. Grafana Labs imposes usage limits for this service.</p>
-                  <p>Additionally, the following terms (&quot;AI Terms&quot;) are hereby added to and become part of your licensing agreement with Grafana Labs (the &quot;Agreement&quot;) as additional terms. Capitalized terms not defined in these AI Terms have the meanings given in the Agreement. These terms apply to your specific use of the OpenAI via Grafana Labs feature(s), and are separate, necessary terms regarding your use of this feature and therefore are not &apos;click-wrap&apos;, &apos;shrink-wrap&apos;, different or additional terms, or the like, to the extent your licensing agreement with Grafana Labs purports to supersede any such terms.</p>
-                  <ul>
-                    <li>Grafana Labs uses OpenAI&apos;s API platform to provide the LLM features.</li>
-                    <li>OpenAI does not train aggregated models on inputs or outputs of the API platform as used in connection with Grafana Labs Product(s).</li>
-                    <li>OpenAI does retain data for a short time in order to provide the services and monitor for abuse. All data sent to OpenAI is encrypted in transit and at rest.</li>
-                    <li>All features utilizing OpenAI are clearly marked in the Grafana Labs Product(s), and each feature sends minimal data to OpenAI&mdash;and only at the request of a user (for example, when a user clicks the button to request an Incident auto-summary).</li>
-                    <li>Grafana Labs will add new features regularly that utilize features connecting to OpenAI&apos;s APIs, which may include, but are not limited to:
-                      <ul>
-                        <li>Explaining Flamegraphs & offer suggestions to fix issues</li>
-                        <li>Incident auto-summary</li>
-                        <li>Suggesting names & descriptions for panels & dashboards, and summarize differences when saving changes</li>
-                        <li>Explaining error log lines in Sift</li>
-                        <li>Generating KQL queries in the Azure Data Explorer plugin</li>
-                      </ul>
-                    </li>
-                    <li>
-                      Visit the OpenAI trust portal for more detail about OpenAI:{' '}
-                      <Button
-                        size="sm"
-                        variant="secondary"
-                        onClick={(e) => window.open('https://trust.openai.com/', '_blank')}
-                      >
-                        https://trust.openai.com/
-                      </Button>
-                    </li>
-                    <li>
-                      If you enable this feature, OpenAI will be a subprocessor of Grafana Labs for the purpose of any data processing agreement you may have in place with Grafana Labs.
-                    </li>
-                    <li>
-                      Disclaimer. Outputs are generated through machine learning processes and are not tested, verified, endorsed or guaranteed to be accurate, complete or current by Grafana Labs. You should independently review and verify all outputs as to appropriateness for any or all of your use cases or applications. The warranties, disclaimers, and limitations of liability in the Agreement apply to the AI Features.
-                    </li>
-                  </ul>
-                </div>
-                <Checkbox
-                  value={optIn}
-                  onClick={optInChange}
-                  label="I accept the AI Terms above"
-                />
-              </>
-            )}
-          </Card.Description>
-          <Card.Figure>
-            <Icon name="grafana" size="lg" />
-          </Card.Figure>
-        </Card>
+        <div onClick={selectGrafanaManaged}>
+          <Card
+            isSelected={llmOption === 'grafana-provided'}
+            // onClick={selectGrafanaManaged} // prevents events passing to children, use parent div instead!
+            className={s.cardWithoutBottomMargin}
+          >
+            <Card.Heading>Use OpenAI provided by Grafana</Card.Heading>
+            <Card.Description>
+              <p>Enable LLM features in Grafana by using a connection to OpenAI that is provided by Grafana</p>
+              {llmOption === 'grafana-provided' && (
+                <>
+                  <div className={s.openaiTermsBox}>
+                    <p>
+                      To enable OpenAI via Grafana Labs, please note that some data from your Grafana instance will be
+                      sent to OpenAI when you use the LLM-based features. Grafana Labs imposes usage limits for this
+                      service.
+                    </p>
+                    <p>
+                      Additionally, the following terms (&quot;AI Terms&quot;) are hereby added to and become part of
+                      your licensing agreement with Grafana Labs (the &quot;Agreement&quot;) as additional terms.
+                      Capitalized terms not defined in these AI Terms have the meanings given in the Agreement. These
+                      terms apply to your specific use of the OpenAI via Grafana Labs feature(s), and are separate,
+                      necessary terms regarding your use of this feature and therefore are not &apos;click-wrap&apos;,
+                      &apos;shrink-wrap&apos;, different or additional terms, or the like, to the extent your licensing
+                      agreement with Grafana Labs purports to supersede any such terms.
+                    </p>
+                    <ul>
+                      <li>Grafana Labs uses OpenAI&apos;s API platform to provide the LLM features.</li>
+                      <li>
+                        OpenAI does not train aggregated models on inputs or outputs of the API platform as used in
+                        connection with Grafana Labs Product(s).
+                      </li>
+                      <li>
+                        OpenAI does retain data for a short time in order to provide the services and monitor for abuse.
+                        All data sent to OpenAI is encrypted in transit and at rest.
+                      </li>
+                      <li>
+                        All features utilizing OpenAI are clearly marked in the Grafana Labs Product(s), and each
+                        feature sends minimal data to OpenAI&mdash;and only at the request of a user (for example, when
+                        a user clicks the button to request an Incident auto-summary).
+                      </li>
+                      <li>
+                        Grafana Labs will add new features regularly that utilize features connecting to OpenAI&apos;s
+                        APIs, which may include, but are not limited to:
+                        <ul>
+                          <li>Explaining Flamegraphs & offer suggestions to fix issues</li>
+                          <li>Incident auto-summary</li>
+                          <li>
+                            Suggesting names & descriptions for panels & dashboards, and summarize differences when
+                            saving changes
+                          </li>
+                          <li>Explaining error log lines in Sift</li>
+                          <li>Generating KQL queries in the Azure Data Explorer plugin</li>
+                        </ul>
+                      </li>
+                      <li>
+                        Visit the OpenAI trust portal for more detail about OpenAI:{' '}
+                        <Button
+                          size="sm"
+                          variant="secondary"
+                          onClick={(e) => {
+                            window.open('https://trust.openai.com/', '_blank');
+                            e.stopPropagation();
+                          }}
+                        >
+                          https://trust.openai.com/
+                        </Button>
+                      </li>
+                      <li>
+                        If you enable this feature, OpenAI will be a subprocessor of Grafana Labs for the purpose of any
+                        data processing agreement you may have in place with Grafana Labs.
+                      </li>
+                      <li>
+                        Disclaimer. Outputs are generated through machine learning processes and are not tested,
+                        verified, endorsed or guaranteed to be accurate, complete or current by Grafana Labs. You should
+                        independently review and verify all outputs as to appropriateness for any or all of your use
+                        cases or applications. The warranties, disclaimers, and limitations of liability in the
+                        Agreement apply to the AI Features.
+                      </li>
+                    </ul>
+                  </div>
+                  <Checkbox value={optIn} onClick={optInChange} label="I accept the AI Terms above" />
+                </>
+              )}
+            </Card.Description>
+            <Card.Figure>
+              <Icon name="grafana" size="lg" />
+            </Card.Figure>
+          </Card>
+        </div>
       )}
-
       <Card isSelected={llmOption === 'openai'} onClick={selectOpenAI} className={s.cardWithoutBottomMargin}>
         <Card.Heading>Use your own OpenAI account</Card.Heading>
         <Card.Description>
@@ -167,7 +200,6 @@ export function LLMConfig({
           <OpenAILogo width={20} height={20} />
         </Card.Figure>
       </Card>
-
       <Card isSelected={llmOption === 'disabled'} onClick={selectLLMDisabled} className={s.cardWithoutBottomMargin}>
         <Card.Heading>Disable all LLM features in Grafana</Card.Heading>
         <Card.Description>&nbsp;</Card.Description>
@@ -187,19 +219,27 @@ export const getStyles = (theme: GrafanaTheme2) => ({
   nestedList: css`
     margin-left: ${theme.spacing(3)};
   `,
-  openaiTermsBox: css`
-    overflow-y: auto;
-    z-index: 2;
-    margin-right: ${theme.spacing(3)};
-    padding: ${theme.spacing(1)} ${theme.spacing(1)} ${theme.spacing(1)} ${theme.spacing(3)};
-    border: 1px solid ${theme.colors.border.medium};
-    background: ${theme.colors.background.primary};
-    color: ${theme.colors.text.primary};
-  `,
+  openaiTermsBox: css({
+    'overflow-y': 'auto',
+    height: '250px',
+    'margin-right': theme.spacing(3),
+    'margin-bottom': theme.spacing(1),
+    padding: `${theme.spacing(1)} ${theme.spacing(2)} ${theme.spacing(1)} ${theme.spacing(2)}`,
+    border: `1px solid ${theme.colors.border.medium}`,
+    background: theme.colors.background.primary,
+    color: theme.colors.text.primary,
+
+    ' ul': {
+      // space important, matches all children of type 'ul'
+      'padding-left': theme.spacing(2),
+    },
+    '> ul > li:not(:last-child)': {
+      // slight vertical padding between main bullet points
+      'margin-bottom': theme.spacing(0.5),
+    },
+  }),
   cardWithoutBottomMargin: css`
     margin-bottom: 0;
     margin-top: ${theme.spacing(1)};
   `,
-  inlineFieldWidth: 15,
-  inlineFieldInputWidth: 40,
 });
