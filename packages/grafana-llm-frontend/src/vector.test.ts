dcbea680b6577b4bda6d8dafa55d2a52cc134cd6
Aaron Sanders <aaron.sanders@grafana.com>
2024-02-20 15:54:22 -0600

    Minimal separate llm package that seems to build

---
+++ packages/grafana-llm-frontend/src/vector.test.ts
@@ -0,0 +1,37 @@
+import { enabled } from './vector';
+import { LLM_PLUGIN_ROUTE } from './constants';
+import { getBackendSrv } from '@grafana/runtime';
+
+jest.mock('@grafana/runtime', () => ({
+  getBackendSrv: jest.fn(),
+}));
+
+describe('enabled', () => {
+  it('should return false if not configured', async () => {
+    (getBackendSrv as jest.Mock).mockImplementation(() => ({
+      get: jest.fn().mockReturnValue(Promise.resolve({ enabled: false })),
+    }));
+
+    // Call the enabled function
+    const result = await enabled();
+    expect(result).toBe(false);
+  });
+
+  it('should return true if configured', async () => {
+    (getBackendSrv as jest.Mock).mockImplementation(() => ({
+      get: jest.fn().mockImplementation((url: string) => {
+        if (url === `${LLM_PLUGIN_ROUTE}/settings`) {
+          return Promise.resolve({ enabled: true });
+        } else if (url === `${LLM_PLUGIN_ROUTE}/health`) {
+          return Promise.resolve({ details: { vector: { enabled: true, ok: true } } });
+        }
+        // raise an error if we get here
+        throw new Error('unexpected url');
+      }),
+    }));
+
+    // Call the enabled function
+    const result = await enabled();
+    expect(result).toBe(true);
+  });
+});
