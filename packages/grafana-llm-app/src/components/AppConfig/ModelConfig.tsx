7d561726e4e4867f8f18c3fc6ae66bebcef72829
Yasir Ekinci <yas.ekinci@grafana.com>
2024-05-10 14:25:48 +0200

    simplify set default UX + some naming changes

--- packages/grafana-llm-app/src/components/AppConfig/ModelConfig.tsx
+++ packages/grafana-llm-app/src/components/AppConfig/ModelConfig.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Field, FieldSet, Input, Label, Select } from '@grafana/ui';
+import { Badge, Button, Divider, Field, FieldSet, Input, Label } from '@grafana/ui';
 import { openai } from '@grafana/llm';
 
 import { OpenAIProvider } from './OpenAI';
@@ -13,38 +13,36 @@ export interface ModelSettings {
 }
 
 export interface ModelMappingConfig {
-  model: openai.Model;
+  id: openai.Model;
   name: string;
   label: string;
   description: string;
 }
-const DEFAULT_MODEL = openai.Model.BASE;
-const DEFAULT_MODEL_NAMES: ModelMappingConfig[] = [
+const DEFAULT_MODEL_ID = openai.Model.BASE;
+const MODEL_MAPPING_CONFIG: ModelMappingConfig[] = [
   {
-    model: openai.Model.BASE,
+    id: openai.Model.BASE,
     name: 'gpt-3.5-turbo',
     label: 'Base',
     description: 'A fast and cost-effective model for efficient, high-throughput tasks.',
   },
   {
-    model: openai.Model.LARGE,
+    id: openai.Model.LARGE,
     name: 'gpt-4-turbo',
     label: 'Large',
-    description:
-      'A larger, higher cost model for more advanced tasks with longer context windows.',
+    description: 'A larger, higher cost model for more advanced tasks with longer context windows.',
   },
 ];
 
 const initModelSettings = (settings: ModelSettings): ModelSettings => ({
-  default: settings.default ?? DEFAULT_MODEL,
+  default: settings.default ?? DEFAULT_MODEL_ID,
   // If the settings are empty, set the default models
   // If the settings are not empty, filter out any models that are not in the default list
   mapping: settings.mapping
-    ? Object.fromEntries(
-      Object.entries(settings.mapping)
-        .filter(([m, _]) => DEFAULT_MODEL_NAMES.find((d) => d.model === m))
-    ) as ModelMapping
-    : Object.fromEntries(DEFAULT_MODEL_NAMES.map((entry) => ([entry.model, entry.name]))) as ModelMapping,
+    ? (Object.fromEntries(
+        Object.entries(settings.mapping).filter(([m, _]) => MODEL_MAPPING_CONFIG.find((d) => d.id === m))
+      ) as ModelMapping)
+    : (Object.fromEntries(MODEL_MAPPING_CONFIG.map((entry) => [entry.id, entry.name])) as ModelMapping),
 });
 
 export function ModelConfig({
@@ -57,53 +55,64 @@ export function ModelConfig({
   onChange: (settings: ModelSettings) => void;
 }) {
   settings = initModelSettings(settings);
+  const setDefault = (model: openai.Model) => onChange({ ...settings, default: model });
 
   return (
     <FieldSet>
-      <Field
-        label="Default Model"
-        description="The default model is used when no model is specified in the chat request."
-      >
-        <Select
-          options={DEFAULT_MODEL_NAMES.map((entry) => ({ label: entry.label, value: entry.model }))}
-          width={60}
-          value={settings.default ?? DEFAULT_MODEL}
-          onChange={(e) => onChange({ ...settings, default: e.value ?? openai.Model.BASE })}
-        />
-      </Field>
-
       {/*
         Only show custom model mappings for non-Azure providers.
         When using the Azure provider users can just customise the deployments
         instead.
-      */ }
-      {provider !== "azure" && (
+      */}
+      {provider !== 'azure' && (
         <>
-          <Label description="Set custom models used for LLM features.">Custom overrides</Label>
-          {DEFAULT_MODEL_NAMES.map((entry, i) => {
-            const modelSetting = settings.mapping[entry.model];
+          <Label description="Map custom models used for LLM features. The default model is used when no model is specified in the chat request.">
+            Model mappings
+          </Label>
+          {MODEL_MAPPING_CONFIG.map((entry, i) => {
+            const modelSetting = settings.mapping[entry.id];
             // If the model is not in the settings, add it with the default name
             if (!modelSetting) {
-              onChange({ ...settings, mapping: { ...settings.mapping, [entry.model]: entry.name } });
+              onChange({ ...settings, mapping: { ...settings.mapping, [entry.id]: entry.name } });
             }
+            const isDefault = settings.default === entry.id;
+            const FieldLabel = (
+              <>
+                <Label>
+                  {entry.label}
+                  <Divider direction="vertical" spacing={1} />
+                  {isDefault ? (
+                    <div style={{ margin: '1px auto' }}>
+                      <Badge text="Default" color="blue" />
+                    </div>
+                  ) : (
+                    <Button variant="secondary" size="sm" onClick={() => setDefault(entry.id)}>
+                      Set as default
+                    </Button>
+                  )}
+                </Label>
+              </>
+            );
 
             return (
-              <Field key={i} label={`${entry.label}`} description={entry.description}>
-                <Input
-                  width={60}
-                  type="text"
-                  name="model"
-                  value={modelSetting ?? entry.name}
-                  onChange={(e) => {
-                    const newModelName = e.currentTarget.value;
-                    const newMapping = {
-                      ...settings.mapping,
-                      [entry.model]: newModelName,
-                    };
-                    onChange({ ...settings, mapping: newMapping });
-                  }}
-                />
-              </Field>
+              <>
+                <Field key={i} label={FieldLabel} description={entry.description}>
+                  <Input
+                    width={60}
+                    type="text"
+                    name="model"
+                    value={modelSetting ?? entry.name}
+                    onChange={(e) => {
+                      const newModelName = e.currentTarget.value;
+                      const newMapping = {
+                        ...settings.mapping,
+                        [entry.id]: newModelName,
+                      };
+                      onChange({ ...settings, mapping: newMapping });
+                    }}
+                  />
+                </Field>
+              </>
             );
           })}
         </>
